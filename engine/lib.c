ec690df570c2e8687808ad79d749cd658473c6eb
Little <Little@DESKTOP-9K91L1P>
2016-12-16 14:06:24 +0800

    Test the case see if it is correct to encrypt

---
+++ engine/lib.c
@@ -0,0 +1,69 @@
+﻿#include "lib.h"
+#include "ctx.h"
+
+
+
+NTSTATUS
+File_EncryptBuffer(
+	__in PVOID buffer,
+	__in ULONG Length,
+	__in PCHAR PassWord,
+	__inout PUSHORT CryptIndex,
+	__in ULONG ByteOffset
+)
+{
+	PCHAR buf;
+	UINT64 i;
+
+	UNREFERENCED_PARAMETER(PassWord);
+
+	buf = (PCHAR)buffer;
+
+	//if (NULL != CryptIndex)
+	//{
+	//if (*CryptIndex == CRYPT_INDEX_BIT)
+	//{
+	for (i = 0; i<Length; i++)
+	{
+		buf[i] ^= ENC_BUFF_KEY_BIT;
+	}
+	// } else {
+	//ÆäËû¼ÓÃÜËã·¨
+	// }
+	//}
+
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS
+File_DecryptBuffer(
+	__in PVOID buffer,
+	__in ULONG Length,
+	__in PCHAR PassWord,
+	__inout PUSHORT CryptIndex,
+	__in ULONG ByteOffset
+)
+{
+	PCHAR buf;
+	UINT64 i;
+
+	UNREFERENCED_PARAMETER(PassWord);
+
+	buf = (PCHAR)buffer;
+
+	//if (NULL != CryptIndex)
+	//{
+	//if (*CryptIndex == CRYPT_INDEX_BIT)
+	//{
+	for (i = 0; i<Length; i++)
+	{
+		buf[i] ^= ENC_BUFF_KEY_BIT;
+	}
+	// } else {
+	//ÆäËû¼ÓÃÜËã·¨
+	// }
+	//}
+
+	return STATUS_SUCCESS;
+}
+
---
+++ engine/lib.h
@@ -0,0 +1,58 @@
+﻿#ifndef _LIB_H_
+#define _LIB_H_
+
+#include "common.h"
+
+//Òì»ò¼ÓÃÜ
+#define CRYPT_INDEX_BIT 1
+#define CRYPT_FUNC_INDEX sizeof(UCHAR)
+#define ENC_BUFF_KEY_BIT 0x77
+
+
+
+VOID
+Cc_ClearFileCache1(
+	__inout PFLT_CALLBACK_DATA Data,
+	__in PCFLT_RELATED_OBJECTS FltObjects,
+	__deref_out_opt PVOID *CompletionContext
+
+);
+
+
+
+/**
+* ¼ÓÃÜ»º´æÄÚÈÝ
+*buffer:Ö¸Ïò»º´æµÄÖ¸Õë
+*Length:»º´æ³¤¶È
+*PassWord:ÃÜÂë£¬Ä¿Ç°Î´Ê¹ÓÃ
+*CryptIndex:¼ÓÃÜ½âÃÜËã·¨Ë÷ÒýºÅ
+*/
+NTSTATUS
+File_EncryptBuffer(
+	__in PVOID buffer,
+	__in ULONG Length,
+	__in PCHAR PassWord,
+	__inout PUSHORT CryptIndex,
+	__in ULONG ByteOffset
+);
+
+/**
+* ½âÃÜ»º´æÄÚÈÝ
+*buffer:Ö¸Ïò»º´æµÄÖ¸Õë
+*Length:»º´æ³¤¶È
+*PassWord:ÃÜÂë£¬Ä¿Ç°Î´Ê¹ÓÃ
+*CryptIndex:¼ÓÃÜ½âÃÜËã·¨Ë÷ÒýºÅ
+*/
+NTSTATUS
+File_DecryptBuffer(
+	__in PVOID buffer,
+	__in ULONG Length,
+	__in PCHAR PassWord,
+	__inout PUSHORT CryptIndex,
+	__in ULONG ByteOffset
+);
+
+
+
+
+#endif
